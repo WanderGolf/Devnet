7c1659c0fc4834d108966e141b5b9e5303aaa32f
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2020-12-29 14:09:26 +0000

    part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,3 +14,4 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
