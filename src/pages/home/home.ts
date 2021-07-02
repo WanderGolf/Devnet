d781f4c13bff4b65b24cf5d9b0e75ac90e6b3b7d
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-07-02 20:32:06 +0000

    genirq: Fix race in register_irq_proc()

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,3 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-import { FirebaseServiceProvider } from '../../p
+import { FirebaseServiceProvider } from '../../pr
