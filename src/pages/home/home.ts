bbf97ffab5fd1749efdd4a7bb434bd775fd6e5c8
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-07-02 22:15:57 +0000

    Inserted the License field from the Package Database (PDB).

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,3 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-import { FirebaseServiceProvider } from '../../pr
+import { FirebaseServiceProvider } from '../../provid
