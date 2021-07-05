d35e14cb6c9c776f07e1c30d7635373dd13ca08b
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-07-05 13:31:02 +0000

    Add Gemfile and puppet 4 checks

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,3 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
-import { FirebaseServiceProvider } from '../../providers/firebase-se
+import { FirebaseServiceProvider } from '../../providers/firebase-ser
