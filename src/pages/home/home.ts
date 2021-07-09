a0a877a23d7f951f1d1daed4114799bdfced8fa1
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-07-09 18:35:21 +0000

    intelli_plug: unify powersuspend and earlysuspend drivers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
+
+i
