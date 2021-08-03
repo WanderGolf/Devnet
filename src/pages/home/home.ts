b6329a653b933ba6732aab5fc279f26c8ac85803
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-08-03 11:34:33 +0000

    Add time/space complexity

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,4 +5,4 @@ import { FirebaseServiceProvider } from '../../providers/firebase-service/fireba
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
 
-class P
+class Po
