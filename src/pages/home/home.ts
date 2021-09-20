49df9b1bcf72322502d9843fb4c7aae521620bd8
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-09-20 21:47:35 +0000

    Update logout.php

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,4 +16,4 @@ class Post {
   templateUrl: 'home.html'
 })
 export class HomePage {
-  ToDoListItems: Observable<any>; //FirebaseListObservab
+  ToDoListItems: Observable<any>; //FirebaseListObservable
