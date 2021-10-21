7a1e215ee2775c20595c6ea9736deb619707cf35
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-10-21 12:52:33 +0000

    Added a few tests

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,4 +21,4 @@ export class HomePage {
   newItem = '';
   postid: any;
 
-  constructor(public navCtrl: NavController, public firebase
+  constructor(public navCtrl: NavController, public firebaseSe
