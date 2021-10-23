cc50d83a852efc4ff6292665fddde618d114e327
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-10-23 21:14:10 +0000

    clustering

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,4 +21,4 @@ export class HomePage {
   newItem = '';
   postid: any;
 
-  constructor(public navCtrl: NavController, public firebaseService: Firebase
+  constructor(public navCtrl: NavController, public firebaseService: FirebaseS
