679ed5512039b3a56d56ba05cc364dc19d8e54ca
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-10-26 22:48:27 +0000

    cleaned repo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,4 +21,4 @@ export class HomePage {
   newItem = '';
   postid: any;
 
-  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,pr
+  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,privat
