fb8f8137d55facc9a19fce879005d0cc252cd34a
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-10-28 09:49:10 +0000

    Fixed failing unit tests

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,4 +21,4 @@ export class HomePage {
   newItem = '';
   postid: any;
 
-  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,private database: 
+  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,private database: A
