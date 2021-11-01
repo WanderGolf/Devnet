80410ff5ae7e9dcdecfd7e2fe6f05f9c94a722a7
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-11-01 13:38:06 +0000

    Conflict resolution

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,4 +21,4 @@ export class HomePage {
   newItem = '';
   postid: any;
 
-  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,private database: AngularFireDat
+  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,private database: AngularFireData
