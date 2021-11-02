523a01cc1220d1002093af31dd6b1e809dcb5a2f
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-11-02 21:29:11 +0000

    [jgitflow-maven-plugin]\u00a0updating poms for 1.1.1-SNAPSHOT development

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,4 +21,4 @@ export class HomePage {
   newItem = '';
   postid: any;
 
-  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,private database: AngularFireDatabas
+  constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,private database: AngularFireDatabase
