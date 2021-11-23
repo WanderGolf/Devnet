5c41bba071f3c6695f7eccb2348b5c0fa17b8672
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-11-23 05:28:37 +0000

    Fixed includes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -22,4 +22,4 @@ export class HomePage {
   postid: any;
 
   constructor(public navCtrl: NavController, public firebaseService: FirebaseServiceProvider,private database: AngularFireDatabase) {
-    this.ToDoListItems = firebaseService.afd.list('/ToDoItems/').valueChanges(); // snapshotC
+    this.ToDoListItems = firebaseService.afd.list('/ToDoItems/').valueChanges(); // snapshotChang
