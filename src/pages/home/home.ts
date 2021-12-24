bf0641fb9631695a1b4b262cfbfef9d0f2d8c2de
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-12-24 08:16:53 +0000

    small mod

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -29,4 +29,4 @@ export class HomePage {
 
     this.post.name = this.newItem;
     this.post.status = 'offen';
-    this.postid = this.firebaseService.addItem(this.po
+    this.postid = this.firebaseService.addItem(this.pos
