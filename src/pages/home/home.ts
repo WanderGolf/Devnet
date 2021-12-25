b08f3e63f190c7def0ee07c13cbb4142287c56a0
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2021-12-25 12:36:11 +0000

    add some doc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -29,4 +29,4 @@ export class HomePage {
 
     this.post.name = this.newItem;
     this.post.status = 'offen';
-    this.postid = this.firebaseService.addItem(this.pos
+    this.postid = this.firebaseService.addItem(this.post)
