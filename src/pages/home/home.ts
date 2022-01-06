6d6cde70637670ac5c1f509d3e6cd7fbe4455eb4
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2022-01-06 15:12:09 +0000

    Version 2.4.2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,4 +31,4 @@ export class HomePage {
     this.post.status = 'offen';
     this.postid = this.firebaseService.addItem(this.post);
 
-    var brandRef = this.firebaseService.afd.database.ref('/ToDoItems/'+this.pos
+    var brandRef = this.firebaseService.afd.database.ref('/ToDoItems/'+this.posti
