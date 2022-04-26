6038d0cab1b0881c94ab82c819ced2880c7948c8
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2022-04-26 08:37:24 +0000

    Update teststyles.css

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -41,3 +41,5 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
             op = body.call(thisArg, _);
         } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
