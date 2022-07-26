035a707ea151ec9d7a7c83dd68544749e8b6aaf1
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2022-07-26 16:15:36 +0000

    edit package.json

--- src/pages/login/login.module.js
+++ src/pages/login/login.module.js
@@ -7,3 +7,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { LoginPage } from './login';
+import { AngularFireAuthProvider } from 'angularfire2/auth';
+var LoginPageModule = /** @class */ (function () {
