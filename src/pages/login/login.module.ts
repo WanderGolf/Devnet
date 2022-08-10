494080d315142a10f544fb4e70b4f457e269b804
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2022-08-10 18:15:13 +0000

    Add fields

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,3 +1,7 @@
 
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { LoginPage } from './login';
+import { AngularFireAuthProvider } from 'angularfire2/auth';
+
+@NgModule({
