454fb1da566b60e4d64c0b543391b6d77b2a3116
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2022-09-07 22:28:40 +0000

    update some files

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,3 +3,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from "../../shared/models/user";
 import { AngularFireAuth } from 'angularfire2/auth';
+import { HomePage } from '../home/home';
+
+@IonicPage()
