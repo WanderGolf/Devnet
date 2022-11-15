af21f68ddb9b5c79be918bec58e8e51f1dec2ace
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2022-11-15 10:47:09 +0000

    md/raid1: consider WRITE as successful only if at least one non-Faulty and non-rebuilding drive completed it.

--- src/providers/firebase-service/firebase-service.ts
+++ src/providers/firebase-service/firebase-service.ts
@@ -2,3 +2,6 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { AngularFireDatabase } from 'angularfire2/database';
+
+/*
+  Generated class for the FirebaseServiceProvider provider.
