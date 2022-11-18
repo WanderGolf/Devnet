2177747d52c15a727663a9619ab90ce43c9ed38c
WanderGolf <104527097+WanderGolf@users.noreply.github.com>
2022-11-18 12:11:17 +0000

    v0.1.17

--- src/providers/firebase-service/firebase-service.ts
+++ src/providers/firebase-service/firebase-service.ts
@@ -9,3 +9,6 @@ import { AngularFireDatabase } from 'angularfire2/database';
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
+@Injectable()
+export class FirebaseServiceProvider {
+  constructor(public afd: AngularFireDatabase) {
